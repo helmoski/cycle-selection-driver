dde2c2473a27a46d44f4319aca09ce3e17d7a7cd
Ryan Helmoski <ryanhelmoski@gmail.com>
2018-01-16 01:00:20 -0600

    Fix lint error

--- src/util/getDescendantTextNodes.ts
+++ src/util/getDescendantTextNodes.ts
@@ -1,7 +1,7 @@
 export function getDescendantTextNodes(document: Document, ancestor: Node): Text[] {
   const textNodes = [] as Text[];
   const treeWalker = document.createTreeWalker(ancestor, NodeFilter.SHOW_TEXT);
-  while (treeWalker.nextNode()) {
+  while (treeWalker.nextNode() as any) {
     textNodes.push(treeWalker.currentNode as Text);
   }
   return textNodes;
