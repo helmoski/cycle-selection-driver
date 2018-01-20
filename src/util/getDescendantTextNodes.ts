75828870cf2b1f1d31c4262b796ea7ec38522770
Ryan Helmoski <ryanhelmoski@gmail.com>
2018-01-20 10:36:07 -0600

    Add unit test for getDescendantTextNodes

--- src/util/getDescendantTextNodes.ts
+++ src/util/getDescendantTextNodes.ts
@@ -1,6 +1,6 @@
-export function getDescendantTextNodes(document: Document, ancestor: Node): Text[] {
+export function getDescendantTextNodes(document: Document, root: Node): Text[] {
   const textNodes = [] as Text[];
-  const treeWalker = document.createTreeWalker(ancestor, NodeFilter.SHOW_TEXT);
+  const treeWalker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT);
   while (treeWalker.nextNode() as any) {
     textNodes.push(treeWalker.currentNode as Text);
   }
