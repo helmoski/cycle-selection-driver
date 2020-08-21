9a0a4f7cc7676ac519819da5138ec33284d043a0
Ryan Helmoski <Ryan.Helmoski1@ibm.com>
2020-08-20 22:36:08 -0400

    Unit test modifySelection

--- src/modifySelection/__tests__/modifySelection.spec.ts
+++ src/modifySelection/__tests__/modifySelection.spec.ts
@@ -1,141 +1,167 @@
-// /* tslint:disable:no-unused-expression object-literal-sort-keys */
+import { clearCurrentSelection } from '../clearCurrentSelection';
+import { getNodeBasedOnRangeNode } from '../getNodeBasedOnRangeNode';
+import { getTargetLeafNodeWithOffset } from '../getTargetLeafNodeWithOffset';
+import { modifySelection } from '../modifySelection';
+import { selectRange } from '../selectRange';
 
-// import { expect } from 'chai';
-// import { SinonStub, spy, stub } from 'sinon';
-// import xstream, { Listener, Stream } from 'xstream';
+jest.mock('../clearCurrentSelection')
+jest.mock('../getNodeBasedOnRangeNode');
+jest.mock('../getTargetLeafNodeWithOffset');
+jest.mock('../selectRange');
 
-// import 'mocha';
+describe('modifySelection', () => {
+  describe('if there is not a new target selection range', () => {
+    it('clears the current selection', () => {
+      modifySelection(null);
+      expect(clearCurrentSelection).toHaveBeenCalledWith();
+    });
 
-// import { IRange } from '../ISelectionRange';
-// import { ISelectionSource } from '../ISelectionSource';
-// import { modifySelection, setGetTargetTextNode } from '../modifySelection';
-// import { IResult } from '../util/getTargetTextNode';
+    it('does not try to selection a new range', () => {
+      modifySelection(null);
+      expect(selectRange).not.toHaveBeenCalled();
+    });
+  });
 
-// function getFakeNode(): Node {
-//   return {
-//     nodeType: 1,
-//   } as Node;
-// }
+  describe('if there is a new target selection range', () => {
+    const targetRange = {
+      endNode: 'FAKE_END_NODE',
+      endOffset: 3,
+      startNode: 'FAKE_START_NODE',
+      startOffset: 1,
+    };
+    const startRootNode = 'FAKE_START_ROOT_NODE';
+    const endRootNode = 'FAKE_END_ROOT_NODE';
+    const startLeafNodeWithOffset = {
+      node: 'FAKE_START_LEAF_NODE',
+      offset: 1,
+    };
+    const endLeafNodeWithOffset = {
+      node: 'FAKE_END_LEAF_NODE',
+      offset: 5,
+    };
+    
+    beforeEach(() => {
+      (getNodeBasedOnRangeNode as jest.Mock).mockRestore();
+      (getNodeBasedOnRangeNode as jest.Mock)
+        .mockReturnValueOnce(startRootNode)
+        .mockReturnValueOnce(endRootNode);
+      (getTargetLeafNodeWithOffset as jest.Mock).mockRestore();
+      (getTargetLeafNodeWithOffset as jest.Mock)
+        .mockReturnValueOnce(startLeafNodeWithOffset)
+        .mockReturnValueOnce(endLeafNodeWithOffset);
+    });
 
-// describe('modifySelection', () => {
-//   let doc: Document;
-//   let selection: Selection;
-//   let documentRange: Range;
-//   let getTargetTextNode: (document: Document, node: Node, offset: number) => IResult;
-//   const startTextNode = {
-//     node: 'start-text-node',
-//     offset: 0,
-//   };
-//   const endTextNode = {
-//     node: 'end-text-node',
-//     offset: 1,
-//   };
+    it('clears the current selection', () => {
+      modifySelection(targetRange);
+      expect(clearCurrentSelection).toHaveBeenCalledWith();
+    });
 
-//   beforeEach(() => {
-//     doc = {
-//       createRange: stub() as () => Range,
-//       getSelection: stub() as () => Selection,
-//       querySelector: stub() as (selectors: string) => Element,
-//     } as Document;
-//     selection = {
-//       addRange: stub() as (range: Range) => void,
-//       removeAllRanges: stub() as () => void,
-//     } as Selection;
-//     documentRange = {
-//       setStart: stub() as (node: Node, offset: number) => void,
-//       setEnd: stub() as (node: Node, offset: number) => void,
-//     } as Range;
-//     (doc.getSelection as SinonStub).returns(selection);
-//     (doc.createRange as SinonStub).returns(documentRange);
-//     getTargetTextNode = stub();
-//     (getTargetTextNode as SinonStub).onFirstCall().returns(startTextNode);
-//     (getTargetTextNode as SinonStub).onSecondCall().returns(endTextNode);
-//     setGetTargetTextNode(getTargetTextNode);
-//   });
+    it('gets the start node', () => {
+      modifySelection(targetRange);
+      expect(getNodeBasedOnRangeNode).toHaveBeenCalledWith(targetRange.startNode);
+    });
 
-//   describe('when the ranges argument is empty', () => {
-//     it('clears the selection', () => {
-//       modifySelection(doc, []);
-//       expect(selection.removeAllRanges).to.have.been.calledOnce;
-//       expect(selection.addRange).not.to.have.been.called;
-//     });
-//   });
+    it('gets the end node', () => {
+      modifySelection(targetRange);
+      expect(getNodeBasedOnRangeNode).toHaveBeenCalledWith(targetRange.endNode);
+    });
 
-//   describe('when a single range is specified', () => {
-//     describe('that uses node elements for start and end', () => {
-//       it('sets the selection based on those nodes', () => {
-//         const range = {
-//           startNode: getFakeNode(),
-//           startOffset: 0,
-//           endNode: getFakeNode(),
-//           endOffset: 1,
-//         } as IRange;
-//         modifySelection(doc, [range]);
-//         expect(selection.removeAllRanges).to.have.been.calledOnce;
-//         expect((getTargetTextNode as SinonStub).firstCall)
-//           .to.have.been.calledWith(doc, range.startNode, range.startOffset);
-//         expect((getTargetTextNode as SinonStub).secondCall)
-//           .to.have.been.calledWith(doc, range.endNode, range.endOffset);
-//         expect(selection.addRange).to.have.been.calledOnce;
-//       });
-//     });
+    describe('if the start node is null', () => {
+      beforeEach(() => {
+        (getNodeBasedOnRangeNode as jest.Mock).mockRestore();
+        (getNodeBasedOnRangeNode as jest.Mock)
+          .mockReturnValueOnce(null)
+          .mockReturnValueOnce(endRootNode);
+      });
 
-//     describe('that uses selector strings for start and end', () => {
-//       it('sets the selection based on the nodes that match those selector strings', () => {
-//         const startNode = getFakeNode();
-//         const endNode = getFakeNode();
-//         (doc.querySelector as SinonStub).onFirstCall().returns(startNode);
-//         (doc.querySelector as SinonStub).onSecondCall().returns(endNode);
-//         const range = {
-//           startNode: 'start',
-//           startOffset: 0,
-//           endNode: 'end',
-//           endOffset: 1,
-//         };
-//         modifySelection(doc, [range]);
-//         expect(selection.removeAllRanges).to.have.been.calledOnce;
-//         expect((doc.querySelector as SinonStub).firstCall)
-//           .to.have.been.calledWith(range.startNode);
-//         expect((doc.querySelector as SinonStub).secondCall)
-//           .to.have.been.calledWith(range.endNode);
-//         expect((getTargetTextNode as SinonStub).firstCall)
-//           .to.have.been.calledWith(doc, startNode, range.startOffset);
-//         expect((getTargetTextNode as SinonStub).secondCall)
-//           .to.have.been.calledWith(doc, endNode, range.endOffset);
-//         expect(selection.addRange).to.have.been.calledOnce;
-//       });
+      it('throws an error', () => {
+        expect.assertions(1);
+        try {
+          modifySelection(targetRange);
+        } catch(e) {
+          expect(e).toEqual(new Error('FAKE_START_NODE does not exist'));
+        }
+      });
+    });
 
-//       describe('and the start does not match any elements', () => {
-//         it('throws an error', () => {
-//           const endNode = getFakeNode();
-//           (doc.querySelector as SinonStub).onFirstCall().returns(null);
-//           (doc.querySelector as SinonStub).onSecondCall().returns(endNode);
-//           const range = {
-//             startNode: 'start',
-//             startOffset: 0,
-//             endNode: 'end',
-//             endOffset: 1,
-//           };
-//           expect(() => modifySelection(doc, [range]))
-//             .to.throw(`${range.startNode} does not exist`);
-//         });
-//       });
+    describe('if the end node is null', () => {
+      beforeEach(() => {
+        (getNodeBasedOnRangeNode as jest.Mock).mockRestore();
+        (getNodeBasedOnRangeNode as jest.Mock)
+          .mockReturnValueOnce(startRootNode)
+          .mockReturnValueOnce(null);
+      });
 
-//       describe('and the end does not match any elements', () => {
-//         it('throws an error', () => {
-//           const startNode = getFakeNode();
-//           (doc.querySelector as SinonStub).onFirstCall().returns(startNode);
-//           (doc.querySelector as SinonStub).onSecondCall().returns(null);
-//           const range = {
-//             startNode: 'start',
-//             startOffset: 0,
-//             endNode: 'end',
-//             endOffset: 1,
-//           };
-//           expect(() => modifySelection(doc, [range]))
-//             .to.throw(`${range.endNode} does not exist`);
-//         });
-//       });
-//     });
-//   });
-// });
+      it('throws an error', () => {
+        expect.assertions(1);
+        try {
+          modifySelection(targetRange);
+        } catch(e) {
+          expect(e).toEqual(new Error('FAKE_END_NODE does not exist'));
+        }
+      });
+    });
+
+    it('gets the start leaf node with offset', () => {
+      modifySelection(targetRange);
+      expect(getTargetLeafNodeWithOffset).toHaveBeenCalledWith(
+        startRootNode,
+        targetRange.startOffset,
+      );
+    });
+
+    it('gets the end leaf node with offset', () => {
+      modifySelection(targetRange);
+      expect(getTargetLeafNodeWithOffset).toHaveBeenCalledWith(
+        endRootNode,
+        targetRange.endOffset,
+      );
+    });
+
+    describe('if the start leaf node with offset is null', () => {
+      beforeEach(() => {
+        (getTargetLeafNodeWithOffset as jest.Mock).mockRestore();
+        (getTargetLeafNodeWithOffset as jest.Mock)
+          .mockReturnValueOnce(null)
+          .mockReturnValueOnce(endLeafNodeWithOffset);
+      });
+
+      it('throws an error', () => {
+        expect.assertions(1);
+        try {
+          modifySelection(targetRange);
+        } catch(e) {
+          expect(e).toEqual(new Error('Start offset out of bounds'));
+        }
+      });
+    });
+
+    describe('if the end leaf node with offset is null', () => {
+      beforeEach(() => {
+        (getTargetLeafNodeWithOffset as jest.Mock).mockRestore();
+        (getTargetLeafNodeWithOffset as jest.Mock)
+          .mockReturnValueOnce(startLeafNodeWithOffset)
+          .mockReturnValueOnce(null);
+      });
+
+      it('throws an error', () => {
+        expect.assertions(1);
+        try {
+          modifySelection(targetRange);
+        } catch(e) {
+          expect(e).toEqual(new Error('End offset out of bounds'));
+        }
+      });
+    });
+
+    it('selects the new range', () => {
+      modifySelection(targetRange);
+      expect(selectRange).toHaveBeenCalledWith(
+        startLeafNodeWithOffset.node,
+        startLeafNodeWithOffset.offset,
+        endLeafNodeWithOffset.node,
+        endLeafNodeWithOffset.offset,
+      );
+    });
+  });
+});
